94d13087929950168ab5cff18dbe8c8238951fdc
islam-mohamad <islam.salaawy@gmail.com>
2022-11-24 09:50:37 +0200

    feat: implement hilt app module

---
+++ app/src/main/java/com/islam/tasks/floawrd/FlowardApp.kt
@@ -0,0 +1,8 @@
+package com.islam.tasks.floawrd
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FlowardApp : Application() {
+}
---
+++ app/src/main/java/com/islam/tasks/floawrd/di/AppModule.kt
@@ -0,0 +1,37 @@
+package com.islam.tasks.floawrd.di
+
+import com.islam.tasks.floawrd.BuildConfig.BASE_URL
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    @Provides
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .client(okHttpClient)
+        .build()
+}
