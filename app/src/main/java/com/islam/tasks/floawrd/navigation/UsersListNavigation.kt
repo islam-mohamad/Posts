a7b49067fb9ae9891578a33846ec97372a6c7189
islam-mohamad <islam.salaawy@gmail.com>
2022-11-24 20:34:15 +0200

    refactor: posts navigation params

--- app/src/main/java/com/islam/tasks/floawrd/navigation/UsersListNavigation.kt
+++ app/src/main/java/com/islam/tasks/floawrd/navigation/UsersListNavigation.kt
@@ -3,18 +3,27 @@ package com.islam.tasks.floawrd.navigation
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.commit
 import com.islam.tasks.floawrd.R
-import com.islam.tasks.posts.PostsFragment
+import com.islam.tasks.posts.presentation.uimodel.PostParam
+import com.islam.tasks.posts.presentation.uimodel.PostUiModel
+import com.islam.tasks.posts.presentation.view.PostsFragment
+import com.islam.tasks.users.presentation.uimodel.UserUiModel
 import com.islam.tasks.users.presentation.view.UsersListNav
 import javax.inject.Inject
 
 class UsersListNavigation @Inject constructor() : UsersListNav {
-    override fun navigateToPosts(activity: FragmentActivity?, userId: Int) {
+    override fun navigateToPosts(activity: FragmentActivity?, user: UserUiModel) {
         activity?.run {
+            val userParam = with(user) {
+                PostParam(
+                    url = url,
+                    thumbnailUrl = thumbnailUrl,
+                    posts = posts.map { PostUiModel(it.id, it.title, it.body) })
+            }
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 replace(
                     R.id.fragment_container_view,
-                    PostsFragment.newInstance(userId)
+                    PostsFragment.newInstance(userParam)
                 )
                 addToBackStack(null)
             }
