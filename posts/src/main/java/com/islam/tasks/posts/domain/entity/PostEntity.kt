d5b89fb8c3216bd1a153d69447fb4c1f6caba197
islam-mohamad <islam.salaawy@gmail.com>
2022-11-24 20:30:08 +0200

    refactor: remove posts domain layer

--- posts/src/main/java/com/islam/tasks/posts/domain/entity/PostEntity.kt
+++
@@ -1,3 +0,0 @@
-package com.islam.tasks.posts.domain.entity
-
-data class PostEntity(var userId: Int, var id: Int, var title: String, var body: String)
--- posts/src/main/java/com/islam/tasks/posts/domain/repository/PostsRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.islam.tasks.posts.domain.repository
-
-import com.islam.tasks.posts.domain.entity.PostEntity
-
-interface PostsRepository {
-    suspend fun getPosts(userId: Int): List<PostEntity>
-}
